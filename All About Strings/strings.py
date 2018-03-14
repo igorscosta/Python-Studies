fb10d7b28350581c814d179936932c6db3ba8326
Igor Costa <igorsc93@gmail.com>
2018-03-14 07:18:13 -0300

    How to create a String

---
+++ All About Strings/strings.py
@@ -0,0 +1,9 @@
+# Strings in Python can be created using single, double or triple quotes
+
+my_string = "Welcome to Python!"
+another_string = 'The bright red fox jumped the fence.'
+a_long_string = '''This is a multi-line string. 
+It covers more than one line'''
+
+#The triple quote line can be done with three single quotes or three double quotes.
+print(a_long_string)
