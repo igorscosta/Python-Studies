039b75897be81620fb1e7544a2afbde16dd93279
Igor Costa <igorsc93@gmail.com>
2018-03-15 05:40:38 -0300

    String Concatenation

--- All About Strings/strings.py
+++ All About Strings/strings.py
@@ -1,3 +1,5 @@
+########### How To Create a String ###############
+
 # Strings in Python can be created using single, double or triple quotes
 
 my_string = "Welcome to Python!"
@@ -6,4 +8,19 @@ a_long_string = '''This is a multi-line string.
 It covers more than one line'''
 
 #The triple quote line can be done with three single quotes or three double quotes.
-print(a_long_string)
+
+
+my_string = "I'm a Python programmer!"
+other_string = 'The word "python" usually refers to a snake'
+triple_string = """Here's anothe way to embed "quotes" in a string"""
+
+#Using the str method
+my_number = 123
+my_string = str(my_number)
+
+############## String Concatenation ###############
+
+string_one = "My dog ate"
+string_two = "my homework!"
+string_three = string_one + string_two
+
