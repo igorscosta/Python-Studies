180dcaa027d3bed2e92224ec3eee29fa06f92688
Igor Costa <igorsc93@gmail.com>
2018-03-18 22:38:13 -0300

    Combining two lists using the extend method

--- Lists, Tuples and Dictionaries/important_data_types.py
+++ Lists, Tuples and Dictionaries/important_data_types.py
@@ -10,3 +10,10 @@ my_list3 = ["a", 1, "Python", 5]
 
 my_nested_list = [my_list, my_list2]
 print(my_nested_list)
+
+# Combining two lists using the extend method
+
+combo_list = []
+one_list = [4,5]
+combo_list.extend(one_list)
+print(combo_list)
